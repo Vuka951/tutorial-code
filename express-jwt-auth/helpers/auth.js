8872029fef8094cebc2b8a5b7c0577a70c1f3e3a
Vuka951 <vultashin76@gmail.com>
2020-02-19 22:44:08 +0100

    error handling added to middleware and routes

--- express-jwt-auth/helpers/auth.js
+++ express-jwt-auth/helpers/auth.js
@@ -5,16 +5,21 @@ const auth = (req, res, next) => {
         const token = req.headers.authorization.split(' ');
         const user = jwt.verify(token[1], 'superScretthing');
         if (token[0] === 'Bearer' && user) {
-            // Also it is comman that if the user is verified, you put the decoded data from the token into req.user to be used later in the code if needed
+            // Also it is common that if the user is verified, you put the decoded data from the token into req.user to be used later in the code if needed
             // Example:
             // req.user = user;
             next()
         }
     } catch (e) {
+        // console.log(e) // Uncomment if needed for debug
+        // If the error thrown is related to JWT
         if (e.name === 'JsonWebTokenError') {
-            res.sendStatus(401).json('JWT Invalid!');
+            res.status(401).json(e.message);
+        // if the authorization header isn't provided
+        } else if(e.name === 'TypeError' && e.message === `Cannot read property 'split' of undefined`) {
+            res.status(400).json(`Please Provide an Authorization Header that contains a token. Example: 'Bearer example_token'`);
         } else {
-            res.sendStatus(401).json('Somethings not right!');
+            res.status(400).json('Something Broke!');
         }
     }
 }
--- express-jwt-auth/index.js
+++ express-jwt-auth/index.js
@@ -7,7 +7,7 @@ const jwt = require('jsonwebtoken');
 // Middleware
 const authMiddle = require('./helpers/auth');
 
-
+// Database connection
 const db = require('knex')({
     client: 'sqlite3',
     connection: {
@@ -27,34 +27,52 @@ app.use(bodyParser.json());
 app.post('/register', async (req, res) => {
     try {
         const {email, password} = req.body;
+
+        if (!email || !password) {
+            res.status(400).json(`Missing ${!email ? "email" : 'password'}!`)
+        }
+
         const hash = await bcrypt.hash(password, 10);
         await db('users').insert({email: email, hash: hash});
+
         const token = jwt.sign({email: email}, 'superScretthing')
         res.status(200).json({token: token});
     } catch(e) {
-        console.log(e);
-        res.status(500).send('Something broke!');
+        // console.log(e); // Uncomment if needed for debug
+        // If a SQLITE_CONSTRAINT has been violated aka. row with that email already exists. You can read more: https://www.sqlite.org/c3ref/c_abort.html
+        if(e.errno === 19) {
+            res.status(400).json('A user with that email already exists!');
+        } else {
+            res.status(400).json('Something broke!');
+        }
     }
 });
 
 app.post('/login', async (req, res) => {
     try {
         const {email, password} = req.body;
+
+        if (!email || !password) {
+            res.status(400).json(`Missing ${!email ? "email" : 'password'}!`)
+        }
+
         const user = await db('users').first('*').where({email: email});
+
         if(user) {
             const validPass = await bcrypt.compare(password, user.hash);
             if(validPass) {
-                const token = jwt.sign({email: email}, 'superScretthing')
+                const token = jwt.sign({email: email}, 'superScretthing');
                 res.status(200).json({token: token});
             } else {
-                res.json('Wrong pass!');
+                res.status(401).json('Wrong password!');
             }
         } else {
             res.status(404).json('User not found!');
         }
+
     } catch(e) {
-        console.log(e);
-        res.status(500).send('Something broke!');
+        // console.log(e); // Uncomment if needed for debug
+        res.status(400).json('Something broke!');
     }
 });
 
