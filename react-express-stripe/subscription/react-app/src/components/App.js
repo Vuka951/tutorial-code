d81660bac33dcc83130a57d44a552c11d7ee63ab
Vuka951 <vultashin76@gmail.com>
2020-03-24 18:16:19 +0100

    .env added to express react strip subscription

--- react-express-stripe/subscription/react-app/src/components/App.js
+++ react-express-stripe/subscription/react-app/src/components/App.js
@@ -7,7 +7,7 @@ import {loadStripe} from '@stripe/stripe-js';
 // Styles
 import '../index.scss';
 
-const stripePromise = loadStripe('pk_test_Oq0kVWwYITwdL0z0nlgGuAEa009E3akd9q');
+const stripePromise = loadStripe(process.env.STRIPE_PUB_KEY);
 
 function App() {
   return (
