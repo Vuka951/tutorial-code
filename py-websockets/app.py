4b31b291c3d37213a1e61a17f6259ee4c78f45ed
Vuka951 <vultashin76@gmail.com>
2020-05-12 19:04:24 +0200

    py websockets added

---
+++ py-websockets/app.py
@@ -0,0 +1,22 @@
+import asyncio
+import websockets
+
+connected = set()
+
+async def server(websocket, path):
+    # Register.
+    connected.add(websocket)
+    try:
+        async for message in websocket:
+            for conn in connected:
+                if conn != websocket:
+                    await conn.send(f'Got a new MSG FOR YOU: {message}')
+    finally:
+        # Unregister.
+        connected.remove(websocket)
+    
+
+start_server = websockets.serve(server, "localhost", 5000)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
